a3109c10588973c30ed6828a6e174f83876a9d9c
Nguyễn Thái An <50405868+thaian229@users.noreply.github.com>
2023-05-23 13:57:50 +0700

    Add 1 type of counter (bàn)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,28 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private float playHeight = 2f;
+    [SerializeField] private float playerRadius = .7f;
+    [SerializeField] private LayerMask countersLayerMask;
 
     private bool isWalking = false;
+    private Vector3 lastInteractDir;
 
-    private void Update()
-    {
+    private void Update() {
+        HandlerMovement();
+        HandlerInteractions();
+    }
+
+    public bool IsWalking() {
+        return isWalking;
+    }
+
+    private void HandlerMovement() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
         Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDirection * Time.deltaTime * moveSpeed;
 
+        float moveDistance = Time.deltaTime * moveSpeed;
+
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playHeight, playerRadius, moveDirection, moveDistance);
+
+        if (!canMove) {
+            // attemp only X movement
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playHeight, playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove) {
+                moveDirection = moveDirectionX;
+            } else {
+                // attemp only Z movement
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playHeight, playerRadius, moveDirectionZ, moveDistance);
+                if (canMove) {
+                    moveDirection = moveDirectionZ;
+                }
+            }
+        }
+
+        if (canMove) {
+            transform.position += moveDirection * moveDistance;
+        }
         isWalking = moveDirection != Vector3.zero;
 
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
     }
 
-    public bool IsWalking()
-    {
-        return isWalking;
+    private void HandlerInteractions() {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if (moveDirection != Vector3.zero) {
+            lastInteractDir = moveDirection;
+        }
+
+        float interactDistance = 2f;
+
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                // has ClearCounter
+                clearCounter.Interact();
+            }
+        } else {
+            Debug.Log("-");
+        }
     }
 }
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -2,27 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
-{
+public class PlayerAnimator : MonoBehaviour {
     private const string IS_WALKING = "IsWalking";
 
     [SerializeField] private Player player;
     [SerializeField] private Animator animator;
 
-    private void Awake()
-    {
+    private void Awake() {
         animator = GetComponent<Animator>();
 
-        if (animator != null && player != null)
-        {
+        if (animator != null && player != null) {
             animator.SetBool(IS_WALKING, player.IsWalking());
         }
     }
 
-    private void Update()
-    {
-        if (animator != null && player != null)
-        {
+    private void Update() {
+        if (animator != null && player != null) {
             animator.SetBool(IS_WALKING, player.IsWalking());
         }
     }
